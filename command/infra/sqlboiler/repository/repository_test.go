c9ae4823f2bfefc672dcacd31afd2c43f832aad4
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-12-17 14:55:04 +0900

    add unit tests for repositoryImpl

---
+++ command/infra/sqlboiler/repository/repository_test.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/zakzackr/grpc-microservices-demo-command-service/infra/sqlboiler/handler"
+)
+
+func TestRepimplPackage(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "infra/sqlboiler/repositoryパッケージのテスト")
+}
+
+// 全テストが実行される前に1度だけ実行される関数
+var _ = BeforeSuite(func() {
+	absPath, _ := filepath.Abs("../../config/database.toml")
+	// database.tomlファイルにパスを環境変数に設定する
+	os.Setenv("DATABSE_TOML_PATH", absPath)
+	err := handler.DBConnect() // データベースに接続する
+	Expect(err).NotTo(HaveOccurred(), "データベース接続が失敗したのでテストを中止します。")
+})
