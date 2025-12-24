0e35f0844532e1b604235531bd896f6b9d460df5
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-12-24 14:43:34 +0900

    support tls

--- command/presen/prepare/register.go
+++ command/presen/prepare/register.go
@@ -1,22 +1,44 @@
 package prepare
 
 import (
+	"crypto/tls"
+	"embed"
+	"io/fs"
+
 	"github.com/zakzackr/grpc-microservices-demo-pb/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-// gRPCサーバ
+// 埋め込むファイル指定
+//
+//go:embed commandservice.pem commandservice-key.pem
+var content embed.FS
+
+// gRPCサーバの生成とServer機能の登録
 type CommandServer struct {
-	Server *grpc.Server
+	Server *grpc.Server // gRPCServer
 }
 
-// コンストラクタ
+// 証明書、秘密鍵をロードする
+func loadPem() credentials.TransportCredentials {
+	cert, _ := fs.ReadFile(content, "commandservice.pem")
+	key, _ := fs.ReadFile(content, "commandservice-key.pem")
+	if certificate, err := tls.X509KeyPair(cert, key); err != nil {
+		panic(err)
+	} else {
+		creds := credentials.NewServerTLSFromCert(&certificate)
+		return creds
+	}
+}
+
+// コンストラクタ TLSを利用
 func NewCommandServer(category pb.CategoryCommandServer, product pb.ProductCommandServer) *CommandServer {
-	// gRPCサーバを生成
-	server := grpc.NewServer()
-	// CategoryCommandServerを登録
+	// gRPCサーバを生成する
+	server := grpc.NewServer(grpc.Creds(loadPem()))
+	// CategoryCommandServerを登録する
 	pb.RegisterCategoryCommandServer(server, category)
-	// ProductCommandServerを登録
+	// ProductCommandServerを登録する
 	pb.RegisterProductCommandServer(server, product)
 	return &CommandServer{Server: server}
 }
